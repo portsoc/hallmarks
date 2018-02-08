c3d3e49cc4bb6924197b87e61cfe896b02eb3a0f
Rich Boakes <rjb@port.ac.uk>
2018-02-08 01:05:32 +0000

    Add subtle colour fading.

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ window.addEventListener("load", () => {
   let rot = 0;
   let step = 30;
   let lis = document.querySelectorAll("li");
-  for (let li of lis) {
-    if (!li.id) {
-      li.style = `filter: hue-rotate(${rot}deg)`
-      rot += step;      
+  function recolour() {
+    let nextRot = rot+1;
+    for (let li of lis) {
+      if (!li.id) {
+        li.style = `filter: hue-rotate(${rot}deg)`
+        rot += step;
+      }
     }
+    rot=nextRot;
   }
+
+  setInterval(recolour,1000);
   window.x.setAttribute("style", `filter: brightness(100%)`);
 });
